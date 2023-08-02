7690914df9ff05a0cd35604ebc859818183ddec2
Daniel Gregorio Reyes Alvarez <daniel.reyes@grupocorinsa.hn>
2023-08-02 11:59:47 -0600

    Adding project files

---
+++ lib/presentation/screens/register_screen.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:forms_app/presentation/widgets/widgets.dart';
+import 'package:forms_app/presentation/blocs/register/register_cubit.dart';
+
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Nuevo usuario'),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back_ios_rounded),
+            onPressed: () => context.push('/'),
+          ),
+        ),
+        body: BlocProvider(
+          create: (context) => RegisterCubit(),
+          child: const _RegisterView(),
+        ));
+  }
+}
+
+class _RegisterView extends StatelessWidget {
+  const _RegisterView();
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              FlutterLogo(size: 100),
+              SizedBox(height: 10),
+              _RegisterForm(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _RegisterForm extends StatefulWidget {
+  const _RegisterForm();
+
+  @override
+  State<_RegisterForm> createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<_RegisterForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    final registerCubit = context.watch<RegisterCubit>();
+
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          CustomTextFormField(
+            label: 'Nombre de usuario',
+            onChanged: (value) {
+              registerCubit.usernameChanged(value);
+              _formKey.currentState!.validate();
+            },
+            validator: (value) {
+              if (value == null || value.isEmpty) return 'Campo requerido';
+              if (value.trim().isEmpty) return 'Campo requerido';
+              if (value.length < 6) return 'Mínimo 6 caracteres';
+              return null;
+            },
+          ),
+          const SizedBox(height: 10),
+          CustomTextFormField(
+            label: 'Correo electrónico',
+            onChanged: (value) {
+              registerCubit.emailChanged(value);
+              _formKey.currentState!.validate();
+            },
+            validator: (value) {
+              if (value == null || value.isEmpty) return 'Campo requerido';
+              if (value.trim().isEmpty) return 'Campo requerido';
+
+              final emailRegExp = RegExp(
+                r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+              );
+
+              if (!emailRegExp.hasMatch(value)) return 'Correo inválido';
+              return null;
+            },
+          ),
+          const SizedBox(height: 10),
+          CustomTextFormField(
+            label: 'Contraseña',
+            obscureText: true,
+            onChanged: (value) {
+              registerCubit.passwordChanged(value);
+              _formKey.currentState!.validate();
+            },
+            validator: (value) {
+              if (value == null || value.isEmpty) return 'Campo requerido';
+              if (value.trim().isEmpty) return 'Campo requerido';
+              if (value.length < 6) return 'Mínimo 6 caracteres';
+              return null;
+            },
+          ),
+          const SizedBox(height: 20),
+          FilledButton.tonalIcon(
+            onPressed: () {
+              final isValid = _formKey.currentState!.validate();
+              if (!isValid) return;
+
+              registerCubit.onSubmit();
+            },
+            icon: const Icon(Icons.save),
+            label: const Text('Crear usuario'),
+          ),
+          const SizedBox(height: 20),
+        ],
+      ),
+    );
+  }
+}
